1cd330ac4df0904a0620107aca46017e5d5a038b
khaled mohamed ali <khaled.mhm3d2006@gmail.com>
2025-11-12 23:17:38 +0200

    Add files via upload

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/USER/Desktop/Graph
+# Install script for directory: C:/Users/USER/Desktop/G1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Graph")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/G1")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/USER/Desktop/Graph/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/USER/Desktop/G1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- main.cpp
+++ main.cpp
@@ -9,27 +9,29 @@ int n, m, u, v;
 vector<int> adj[N];
 bool vis[N];
 
-bool isCyclic(int u, int p) { // DFS
+void DFS(int u) {
     vis[u] = true;
-    for (int v : adj[u]) {
-        if (vis[v] && v != p)   return true;
-        else if (!vis[v]) {
-            if (isCyclic(v, u)) return true;
-        }
-    }
-    return false;
+    for (int v : adj[u])
+        if (!vis[v])    DFS(v);
 }
 
 int main() {
+
     cin >> n >> m;
-    int start;
     for (int i = 0; i < m; i++) {
         cin >> u >> v;
         adj[u].push_back(v);
         adj[v].push_back(u);
-        start = u;
     }
-    cout(m == n - 1 && !isCyclic(start, -1) ? "YES" : "NO");
+
+    int connectedComponents = 0;
+    for (int u = 1; u <= n; u++) {
+        if (!vis[u]) {
+            connectedComponents++;
+            DFS(u);
+        }
+    }
+    cout << (connectedComponents == 1 && m == n - 1 ? "YES" : "NO");
     return 0;
 }
 
